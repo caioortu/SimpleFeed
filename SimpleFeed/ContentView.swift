56a9b18f15b8d8e72e4c8d320ae150cc43796ae8
Caio Ortu <caio.ortu@inter.co>
2024-02-15 11:54:46 -0300

    Creating the app

---
+++ SimpleFeed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleFeed
+//
+//  Created by Caio Vasconcelos Ortu on 15/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleFeed/SimpleFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleFeedApp.swift
+//  SimpleFeed
+//
+//  Created by Caio Vasconcelos Ortu on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
